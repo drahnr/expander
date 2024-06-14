3796187edbd5b6fe82af360eddb37195cb7aa1f4
Bernhard Schuster <bernhard@ahoi.io>
2024-06-14 14:12:11 +0200

    chore/test: be verbose and have better assert message

--- tests/baz/lib.rs
+++ tests/baz/lib.rs
@@ -12,8 +12,9 @@ fn baz2(input: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
     };
 
     let expanded = Expander::new("baz")
+        .verbose(true)
         .add_comment("This is generated code!".to_owned())
         .fmt_full(Channel::Stable, Edition::_2021, true)
-        .write_to_out_dir(modified).expect("No IO error");
+        .write_to_out_dir(modified).expect("No IO error happens. qed");
     expanded
 }
